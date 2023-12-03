9394be71b9912de3db037adf3419d1d94b8f8355
mahmood-alselawe <145864552+mahmood-alselawe@users.noreply.github.com>
2023-12-04 01:27:15 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,87 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+/*
+        Variables are places Variables are places reserved in memory to store values
+        (data of a specific type ==> like int , string , char ,long) while the program is running.
+
+        the type we  give to Variables
+        makes the os determine what type should be stored
+
+
+        data have two type:
+
+        1-Primitive Data Types ==> ( (1- byte- short - int - long) -(2-  float - double )- (3- boolean) - (char))
+
+        ==>1- her we order this  Ascending byte =to=> long
+
+        1-byte   8 bits  ,  [-128  , 128] , default value ==> 0
+
+        2-short  16 bits ,  [-32,768 , +32,768] , default value ==> 0
+
+        3-int    32 bits ,  [-2,147,483,647  ,  +2,147,483,647] , default value ==> 0
+
+        4-long   64 bits ,  [-9,223,372,036,854,775,808 , +9,223,372,036,854,775,807] , default value ==> 0L
+
+        ==>2-This type represents a number with a decimal point
+
+        1- float 32 bits , ==> 0.0d if the default value.
+        2- double 64 bits , ==.  0.0d is the default value.
+
+        _________
+
+        3- boolean  ==> i bits ,  true - false  , default values  ==> false
+
+        _________
+
+        4- char  ==> 16 bit  , [0  , 65,535 ] and 'A' and Symbol as value  default values ==> '\u0000'
+
+        __________________
+        __________________
+
+        2-Reference/Object Data Types ==>(Byte _ Short _ Integer _ Float _ Fouble _ Boolean _ Character)
+
+        1- Any type whose parent is an object of a class is considered reference data.
+
+        - so every array in java is reference data
+        - default values for every reference ==> null
+
+        " int i; ==> Declaration , i = 10; ==> Assignment  , int i = 10 ; ==> Initialisation  "
+
+        ---------------
+
+        Operators ==> are symbols that have a specific meaning:
+
+            1-Arithmetic Operators ( = ,  +  , - , * , / , % ,  ++ , -- ,
+
+             +a ==>Multiply the value of a by the factor +  , -a ==> Multiply the value of a by the factor - )
+
+            2-Comparison Operators ((==   ==> use it just with Primitive data ) ,  !=  , >  ,  <  ,  >=  , <=  )
+
+            3-Logical Operators (&& ==> and  , ||  ==> or  , ! ==> not =)
+
+            int a = 10;
+            if( !(a == 10) )
+               {
+                    System.out.println("The condition return false");
+               }
+
+            4-Bitwise Operators https://harmash.com/tutorials/java/operators/bitwize
+
+
+            5-Assignment Operators (= ,( += , -= , *= , /=  %=) ==> i = i + 1 )
+         */
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+    }
+}
